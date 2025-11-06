4efa82dce64fb7a2b22c8699f4050d6c494089ea
brynde <brynde@gmail.com>
2025-11-06 18:42:04 +0200

    complete task siistimpi pelaajalista

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,29 @@
+import requests # type: ignore
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        
+        player = Player(player_dict)
+        players.append(player)
+
+    wanted_nationality = "FIN"
+    national_players = []
+    print(wanted_nationality)
+
+    for player in players:
+        if player.nationality == wanted_nationality:
+            national_players.append(player)
+
+    national_players.sort(key=lambda player: player.points, reverse=True)
+
+    for player in national_players:
+        print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.nationality = dict['nationality']
+        self.team = dict['team']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+        self.points = self.goals + self.assists
     
     def __str__(self):
-        return self.name
+        return f"{self.name} {self.nationality} {self.points}"
